14bdbc5266edaf841eb0b30cd9f9c61085a0fa0f
hung.dang <danghung1202@gmail.com>
2019-11-19 11:01:11 +0700

    Add more methods to file io

--- helper/file-io.js
+++ helper/file-io.js
@@ -36,6 +36,67 @@ module.exports = {
                 resolve(true);
             })
         })
+    },
+
+    /*
+        file <string> | <Buffer> | <URL> | <integer> filename or file descriptor
+        data <string> | <Buffer> | <TypedArray> | <DataView>
+        options <Object> | <string>
+            encoding <string> | <null> Default: 'utf8'
+            mode <integer> Default: 0o666
+            flag <string> See support of file system flags. Default: 'a'.
+        callback <Function>
+            err <Error>
+    */
+    appendFile: (file, data, options) => {
+        return new Promise((resolve, reject) => {
+            fs.appendFile(file, data, options, (err) => {
+                if (err) return reject(err);
+                resolve(true);
+            })
+        })
+    },
+
+    /*
+        path <string> | <Buffer> | <URL>
+
+    */
+    exists: (path) => {
+        return new Promise((resolve, reject) => {
+            fs.access(path, fs.constants.F_OK, (err) => {
+                if (err) return resolve(false);;
+                resolve(true);
+            })
+        })
+    },
+
+    /*
+        path <string> | <Buffer> | <URL>
+
+    */
+    mkdir: (path, options) => {
+        return new Promise((resolve, reject) => {
+            fs.mkdir(path, options, (err) => {
+                if (err) return reject(err);;
+                resolve(true);
+            })
+        })
+    },
+
+    /*
+        path <string> | <Buffer> | <URL>
+
+    */
+    mkdirRecursive: (path, options) => {
+        return new Promise((resolve, reject) => {
+            if (options) Object.assign(options, {
+                recursive: true
+            })
+            fs.mkdir(path, options, (err) => {
+                if (err) return reject(err);;
+                resolve(true);
+            })
+        })
     }
 
 }
